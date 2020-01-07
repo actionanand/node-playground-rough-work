a2cf25833a522f1c2dfc35e067b0677a16e15ac9
actionanand <meetanandns@gmail.com>
2020-01-08 00:02:15 +0530

    adv promise

--- 8-promises.js
+++ 8-promises.js
@@ -1,3 +1,4 @@
+
 // const doWorkPromise = new Promise((resolve, reject) => {
 //     setTimeout(() => {
 //         resolve([3,1,7])
@@ -10,17 +11,31 @@
 
 //with argument
 
-const doWorkPromise = (x) => {
+// const doWorkPromise = (x) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             // resolve([3,1,7])
+//             reject('Something went wrong!')
+//         }, x);
+//     });
+// }
+
+// doWorkPromise(2000).then((result) => {
+//     console.log(result);
+// }).catch((error) => {
+//     console.log(error);
+// })
+
+const add = (a, b) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            // resolve([3,1,7])
-            reject('Something went wrong!')
-        }, x);
+            resolve(a+b)
+        }, 2000)
     });
 }
 
-doWorkPromise(2000).then((result) => {
-    console.log(result);
-}).catch((error) => {
-    console.log(error);
-})
+add(3, 7).then((sum) => {
+    console.log(sum);
+}).catch((e) => {
+    console.log(e);
+});
