ac6bb33e8bfa8458740bdf3596279903f0eed68a
actionanand <meetanandns@gmail.com>
2020-01-08 22:43:09 +0530

    Async-await

--- general play/9-async-await.js
+++ general play/9-async-await.js
@@ -2,6 +2,9 @@
 const add = (a, b) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            if (a<0 || b<0) {
+                return reject('Numbers must be positive!');
+            }
             resolve(a+b)
         }, 2000)
     });
@@ -9,7 +12,7 @@ const add = (a, b) => {
 
 const doWork = async () => {
     const sum1 = await add(3, 7);
-    const sum2 = await add(sum1, 5);
+    const sum2 = await add(sum1, -5);
     const sum3 = await add(sum2, 20);
     return sum3;
 }
